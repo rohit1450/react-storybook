abf5ea7b25124534610472f502c8472ab68ac5c9
arush1103 <sharma.arush.hp@gmail.com>
2024-02-02 11:24:56 +0530

    feat: new repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-orange-200 font-bold p-4">Vite + React + Tailwind</h1>
+    </>
+  )
+}
+
+export default App
