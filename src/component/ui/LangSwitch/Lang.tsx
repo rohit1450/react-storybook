82f96ffbf81869a3224ee3d0dbec61dbc0fa74ca
Ritesh-HangingPanda <ritesh.hp.sharma@gmail.com>
2024-02-28 13:03:55 +0530

    feat(lang): save language type into the localstorage

--- src/component/ui/LangSwitch/Lang.tsx
+++ src/component/ui/LangSwitch/Lang.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
-import Select, { StylesConfig, defaultTheme, GetOptionLabel } from 'react-select';
+import { Menu } from '@headlessui/react';
 import english from '../../../assets/english.png';
 import arabic from '../../../assets/arabic.png';
 
@@ -31,69 +31,55 @@ const Lang: React.FC<LangProps> = ({ solutions }) => {
     ];
 
     React.useEffect(() => {
-        const defaultLanguage = languages.find(lang => lang.value === i18n.language);
-        setSelectedLanguage(defaultLanguage || null);
+        const savedLanguage = localStorage.getItem('language');
+        const defaultLanguage = languages.find(lang => lang.value === savedLanguage) || languages[0];
+        setSelectedLanguage(defaultLanguage);
+        i18n.changeLanguage(defaultLanguage.value);
     }, []);
 
     const changeLanguage = (selectedOption: Option | null) => {
         setSelectedLanguage(selectedOption);
         if (selectedOption) {
+            localStorage.setItem('language', selectedOption.value);
             i18n.changeLanguage(selectedOption.value);
         }
     };
 
-    const customStyles: StylesConfig<Option, false> = {
-        control: (baseStyles) => ({
-            ...baseStyles,
-            backgroundColor: 'transparent',
-            color: '#4299e1',
-            border: '1px solid #4299e1'
-        }),
-        menu: (baseStyles) => ({
-            ...baseStyles,
-            border: `1px solid #4299e1`,
-            zIndex: 10,
-            margin: '0px',
-            padding: '0px'
-        }),
-        option: (base) => ({
-            ...base,
-            height: '100%',
-        }),
-    };
-
-    const customTheme = (theme: any) => ({
-        ...theme,
-        margin: '0px',
-        padding: '0px',
-        border: '0px 0px 1px 0px solid #d8ae76 ',
-        colors: {
-            ...defaultTheme.colors,
-            primary25: 'transparent',
-            primary: '#4299e1',
-        },
-    });
-
-    type GetOptionLabel<Option> = (option: Option) => JSX.Element;
-
-    const getOptionLabel: GetOptionLabel<Option> = (option: Option) => (
-        <div className='flex items-center'>
-            <img className='w-7 h-7 mr-2' src={option.switch.src} alt={option.label} />
-        </div>
+    const OptionItem: React.FC<Option> = ({ label, value, switch: { src } }) => (
+        <Menu.Item>
+            {({ active }) => (
+                <button
+                    className={`${active ? 'bg-blue-light text-white' : ''
+                        } group flex items-center w-full px-1 py-1 text-base`}
+                    onClick={() => changeLanguage({ label, value, switch: { src } })}
+                >
+                    <img className="w-7 h-7" src={src} alt={label} />
+                    <span className='mx-2'>{label}</span>
+                </button>
+            )}
+        </Menu.Item>
     );
 
     return (
         <div>
             <div className='inline-block'>
-                <Select
-                    value={selectedLanguage}
-                    onChange={changeLanguage}
-                    options={languages}
-                    styles={customStyles}
-                    theme={customTheme}
-                    getOptionLabel={getOptionLabel}
-                />
+                <Menu as="div" className="relative rounded-md border border-blue-light shadow-lg">
+                    <Menu.Button className="inline-flex justify-start items-center w-full px-1 py-1 text-base text-blue-light font-medium">
+                        {selectedLanguage &&
+                            (
+                                <img className="w-7 h-7 mx-2" src={selectedLanguage.switch.src} alt={selectedLanguage.label} />
+                            )
+                        }
+                        {/* {selectedLanguage ? selectedLanguage.label : 'Select Language'} */}
+                    </Menu.Button>
+                    <Menu.Items>
+                        {languages.map((language, index) => (
+                            <OptionItem key={index} {...language} />
+                        ))}
+                    </Menu.Items>
+                </Menu>
             </div>
+
             <h1 className='text-2xl py-3 font-semibold text-blue-light underline'>{t('Language Changer')}</h1>
             {solutions.map((solution, index) => (
                 <div key={index} className='my-5 flex justify-start items-center gap-5'>
