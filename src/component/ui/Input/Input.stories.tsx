a5103dc30a713da25b6f5cffaf32c2232ec309f8
Ritesh-HangingPanda <ritesh.hp.sharma@gmail.com>
2024-02-29 15:05:17 +0530

    feat: added validation over the the input:text,input:number,input:password

--- src/component/ui/Input/Input.stories.tsx
+++ src/component/ui/Input/Input.stories.tsx
@@ -11,11 +11,13 @@ export default {
 } as Meta;
 
 const Template: StoryFn<InputProps> = (args: InputProps) => <Input {...args} />;
+
 export const StringType: StoryFn<InputProps> = Template.bind({});
 StringType.args = {
   type: "text",
   placeholder: "Enter text",
 };
+
 export const NumberType: StoryFn<InputProps> = Template.bind({});
 NumberType.args = {
   type: "number",
